1110524141abb86d60effffe6b74abc3b9563664
zhangyi <577608433@qq.com>
2016-06-24 21:44:14 +0800

    First Commit

---
+++ Demo/src/com/day5/Test.java
@@ -0,0 +1,17 @@
+package com.day5;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	@org.junit.Test
+	public void testNormal(){
+		String c=null;
+		
+	}
+
+}
